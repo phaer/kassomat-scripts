52f681483913a321d31a21de99c56b94dd13e48f
phaer <hello@phaer.org>
2016-10-08 03:58:15 +0200

    refactor, use some functions.

--- kassomat-set-coin-levels.py
+++ kassomat-set-coin-levels.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python2
-
 import json
 import uuid
 from redis import StrictRedis
+
+
+redis = StrictRedis()
+pubsub = redis.pubsub()
+pubsub.subscribe('hopper-response')
+
 
 def to_int(s):
   try:
     return int(s)
   except ValueError:
     return None
+
 
 def wait_for_message(correlId):
     for msg in pubsub.listen():
@@ -18,80 +24,87 @@ def wait_for_message(correlId):
         if data['correlId'] == correlId:
  	   return data
 
-redis = StrictRedis()
-pubsub = redis.pubsub()
-levels = {}
 
-correlId = str(uuid.uuid4())
-pubsub.subscribe('hopper-response')
-redis.publish('hopper-request', json.dumps({
-    "cmd": "get-all-levels",
-    "msgId": correlId
-}))
-
-msg = wait_for_message(correlId)
-levels = {int(level['value']): int(level['level']) for level in msg['levels']}
-
-
-print("Welcome to kassomat maintenance mode!\n")
-print("The following coins are in the machine:\n")
-for value, count in levels.items():
-    print("%d x %d Eurocent" % (value, count))
-print("""
-Please enter the desired coin value.
- - empty line to quit
- - raw numbers to add coins.
- - prefix "-" to remove coins.
- - prefix "=" to set an absolute amount of coins
-""")
-
-while True:
-    raw_value = raw_input("coin value: ")
-    if raw_value == '':
-	break
-    value = to_int(raw_value)
-    if not value or value not in levels.keys():
-	print("invalid value, valid are:",
-	  ", ".join([str(k) for k in levels.keys()]))
-	continue
-
-    raw_count = raw_input("count: ")
-    
-    if len(raw_count) > 1 and raw_count[0] in ('-', '='):
-        operator = raw_count[0]
-        raw_count = raw_count[1:]
-    else:
-        operator = None
-
-    if raw_count == '':
-	break
-    count = to_int(raw_count)
-    if not count:
-	print("invalid count, please enter an integer.")
-	continue
-
-    if operator == '=':
-        levels[value] = count
-    elif operator == '-':
-        absolute = levels[value] - count
-        levels[value] = absolute if absolute > 0 else 0
-    else:
-        levels[value] = levels[value] + count
-
-    print("new: %d x %d Eurocent" % (value, levels[value]))
-
-    
-print("Sending the following values to the machine:")
-for value, count in levels.items():
-    correlId = str(uuid.uuid4()) 
+def get_levels():
+    correlId = str(uuid.uuid4())
     redis.publish('hopper-request', json.dumps({
-      "cmd": "set-denomination-level",
-      "msgId": correlId,
-      "amount": value,
-      "level": count
+    	"cmd": "get-all-levels",
+   	 "msgId": correlId
     }))
     msg = wait_for_message(correlId)
-    status = 'success' if msg['result'] == 'ok' else 'error'
-    print("%d x %d Eurocent: %s" % (value, levels[value], status))
+    return {int(level['value']): int(level['level']) for level in msg['levels']}
 
-print("Bye.")
+
+def change_levels(levels):
+    print("""
+    Please enter the desired coin value.
+     - empty line to quit
+     - raw numbers to add coins.
+     - prefix "-" to remove coins.
+     - prefix "=" to set an absolute amount of coins
+    """)
+    
+    while True:
+        raw_value = raw_input("> coin value: ")
+        if raw_value == '':
+    	    break
+        value = to_int(raw_value)
+        if not value or value not in levels.keys():
+    	    print("invalid value, valid are:",
+    	    ", ".join([str(k) for k in levels.keys()]))
+    	    continue
+    
+        raw_count = raw_input("> count: ")
+        
+        if len(raw_count) > 1 and raw_count[0] in ('-', '='):
+            operator = raw_count[0]
+            raw_count = raw_count[1:]
+        else:
+            operator = None
+    
+        if raw_count == '':
+    	    break
+        count = to_int(raw_count)
+        if not count:
+    	    print("invalid count, please enter an integer.")
+    	    continue
+    
+        if operator == '=':
+            levels[value] = count
+        elif operator == '-':
+            absolute = levels[value] - count
+            levels[value] = absolute if absolute > 0 else 0
+        else:
+            levels[value] = levels[value] + count
+    
+        print("new: %d x %d Eurocent" % (value, levels[value]))
+    return levels
+
+
+def set_levels(levels): 
+    print("Sending the following values to the machine:")
+    for value, count in levels.items():
+        correlId = str(uuid.uuid4()) 
+        redis.publish('hopper-request', json.dumps({
+            "cmd": "set-denomination-level",
+            "msgId": correlId,
+	    "amount": value,
+	    "level": count
+	}))
+        msg = wait_for_message(correlId)
+        status = 'success' if msg['result'] == 'ok' else 'error'
+        print("%d x %d Eurocent: %s" % (value, levels[value], status))
+
+
+
+if __name__ == '__main__':
+    levels = get_levels()
+    print("Welcome to kassomat maintenance mode!\n")
+    print("The following coins are in the machine:\n")
+    for value, count in levels.items():
+        print("%d x %d Eurocent" % (value, count))
+
+    levels = change_levels(levels)
+    set_levels(levels)
+        
+    print("Bye.")
