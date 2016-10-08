a69493175cc787646507a12adf1cc050155598a8
phaer <hello@phaer.org>
2016-10-08 03:33:44 +0200

    allow relative and negative values\n ...in kassomat-set-coin-level

--- kassomat-set-coin-levels.py
+++ kassomat-set-coin-levels.py
@@ -33,11 +33,17 @@ msg = wait_for_message(correlId)
 levels = {int(level['value']): int(level['level']) for level in msg['levels']}
 
 
-print("Welcome to kassomat maintenance mode!")
-print("The following coins are in the machine:")
+print("Welcome to kassomat maintenance mode!\n")
+print("The following coins are in the machine:\n")
 for value, count in levels.items():
     print("%d x %d Eurocent" % (value, count))
-print("Please enter the amount to add (empty line to finish)")
+print("""
+Please enter the desired coin value.
+ - empty line to quit
+ - raw numbers to add coins.
+ - prefix "-" to remove coins.
+ - prefix "=" to set an absolute amount of coins
+""")
 
 while True:
     raw_value = raw_input("coin value: ")
@@ -50,6 +56,13 @@ while True:
 	continue
 
     raw_count = raw_input("count: ")
+    
+    if len(raw_count) > 1 and raw_count[0] in ('-', '='):
+        operator = raw_count[0]
+        raw_count = raw_count[1:]
+    else:
+        operator = None
+
     if raw_count == '':
 	break
     count = to_int(raw_count)
@@ -57,7 +70,14 @@ while True:
 	print("invalid count, please enter an integer.")
 	continue
 
-    levels[value] = levels[value] + count
+    if operator == '=':
+        levels[value] = count
+    elif operator == '-':
+        absolute = levels[value] - count
+        levels[value] = absolute if absolute > 0 else 0
+    else:
+        levels[value] = levels[value] + count
+
     print("new: %d x %d Eurocent" % (value, levels[value]))
 
     
